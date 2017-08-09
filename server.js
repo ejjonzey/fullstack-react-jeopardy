f34dc8e70e90be1e993f068dae917ea27d93a45b
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-09 14:38:50 -0400

    previous game connected.

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-//this makes a simple hello world express server
 require("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const GamesController = require("./controllers/game");
 const app = express();
+
 mongoose.Promise = global.Promise;
+
+// Mongoose Connection
 mongoose.connect(process.env.MONGODB_URI);
-
 const connection = mongoose.connection;
+
 connection.on('connected', () => {
-  console.log('Mongoose Connected Successfully');    
-});
+  console.log("Mongoose Connected Successfully");
+})
 
-connection.on('error', (err) => {  
-  console.log('Mongoose default connection error: ' + err);
-}); 
-
-
+connection.on('error', (err) => {
+  console.log("Mongoose connection error: " + err);
+})
 
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
+app.use("/api/game", GamesController);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
-})
+  console.log("App is listening on: " + PORT);
+});
